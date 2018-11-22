58f909faf1feea5c779c2fd2c7b0b7aff867c9bf
lt950303 <1459592858@qq.com>
2018-11-23 01:13:30 +0800

    完成了antd的搭建

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import style from'./App.less';
+//引入antd
+import {Icon} from 'antd' 
 
 class App extends Component {
   render() {
     console.log(style);
     return (
       <div className={style.App}>
-        <header className={style['App-header']}>
-          <img src={logo} className={style['App-logo']} alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className={style['App-link']}
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Icon type="retweet" />
       </div>
     );
   }
